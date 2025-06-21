d79b7197d995cb18ffd3c5ce29b68986bbbd6150
Java-batista <java.batista@outlook.com>
2025-06-21 17:59:53 -0300

    dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,10 +7,11 @@ return {
     config = function()
         --require'alpha'.setup(require'alpha.themes.theta'.config)
         local alpha = require("alpha")
-        local dashboard = require("alpha.themes.theta")
+        local theta = require("alpha.themes.theta")
+        local dashboard = require("alpha.themes.dashboard")
 
         -- Set header
-        dashboard.header.val = {
+        theta.header.val = {
             "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
             "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
             "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
@@ -19,6 +20,20 @@ return {
             "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
         }
 
-       alpha.setup(dashboard.config)
+        -- Set menu
+        theta.buttons.val = {
+            { type = "text",    val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+            { type = "padding", val = 1 },
+            dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+            dashboard.button("SPC f f", "󰈞  Find file"),
+            dashboard.button("SPC f g", "󰊄  Live grep"),
+            --dashboard.button("c", "  Configuration", "<cmd>cd stdpath('.config')<CR>"),
+            dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim<CR>"),
+            dashboard.button("p", "  Plugins configuration", "<cmd>Lazy<CR>"),
+            dashboard.button("m", "  Mason configuration", "<cmd>Mason<CR>"),
+            dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+        }
+
+        alpha.setup(theta.config)
     end,
 }
