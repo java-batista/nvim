f3b92c901bb19bc69292a5d2778f484a9b9d8ae9
Java-batista <java.batista@outlook.com>
2025-06-21 21:31:47 -0300

    completions

--- init.lua
+++ init.lua
@@ -1,25 +1,26 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 --require("lazy").setup("plugins")
 require("lazy").setup({
-	-- require 'plugins.catppuccin',
-	-- require 'plugins.tokyonight',
-	require("plugins.treesitter"),
-	require("plugins.telescope"),
-	require("plugins.neo-tree"),
-	require("plugins.lsp-config"),
-	require("plugins.none-ls"),
-	require("plugins.alpha"),
+    -- require 'plugins.catppuccin',
+    -- require 'plugins.tokyonight',
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.neo-tree"),
+    require("plugins.lsp-config"),
+    require("plugins.none-ls"),
+    require("plugins.alpha"),
+    require("plugins.completions"),
 })
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,47 +1,47 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    --tag = "v1.11.0",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    --tag = "v2.0.0-rc.1",
-    lazy = false,
-    opts = {
-      --auto_install = true,
-      ensure_installed = { "lua_ls", "clangd", "pylsp", "arduino_language_server"  },
+    {
+        "williamboman/mason.nvim",
+        --tag = "v1.11.0",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    --tag = "v1.8.0",
-    lazy = false,
-    config = function()
-      --local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    {
+        "williamboman/mason-lspconfig.nvim",
+        --tag = "v2.0.0-rc.1",
+        lazy = false,
+        opts = {
+            --auto_install = true,
+            ensure_installed = { "lua_ls", "clangd", "pylsp", "arduino_language_server" },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        --tag = "v1.8.0",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")        
-               
-      lspconfig.lua_ls.setup({
-        --capabilities = capabilities
-      })
-      lspconfig.clangd.setup({
-        --capabilities = capabilities
-      })
-      lspconfig.pylsp.setup({
-        --capabilities = capabilities
-      })
-      lspconfig.arduino_language_server.setup({
-        --capabilities = capabilities
-      })
+            local lspconfig = require("lspconfig")
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pylsp.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.arduino_language_server.setup({
+                capabilities = capabilities,
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
