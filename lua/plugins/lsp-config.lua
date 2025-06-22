1563735dfcc05875a37b962b56f487c5135d36a2
Java-batista <java.batista@outlook.com>
2025-06-21 22:35:12 -0300

    diagnostic message

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,6 +38,10 @@ return {
                 capabilities = capabilities,
             })
 
+            vim.diagnostic.config({ virtual_text = true, })
+
+            --vim.diagnostic.config({ float = {} })
+
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
