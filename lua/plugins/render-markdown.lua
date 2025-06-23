6fd24dd91ccb91f13273767092883b04c9d198dd
Java-batista <java.batista@outlook.com>
2025-06-23 14:25:50 -0300

    centerpad

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,10 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	opts = {},
+    {
+        "smithbm2316/centerpad.nvim"
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        opts = {},
+    },
 }
